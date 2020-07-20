2d437f9a0f413881daaf7a506a36fb495495393f
sen <sen@136.com>
2020-07-20 08:41:24 +0800

    test

---
+++ src/main/java/com/topnice/demoweb/util/DateUtil.java
@@ -0,0 +1,74 @@
+package com.topnice.demoweb.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+    /**
+     * 时间戳转换成日期格式字符串
+     *
+     * @param seconds 精确到秒的字符串
+     * @return
+     */
+
+    public static String timeStamp2Date(String seconds, String format) {
+        if (seconds == null || seconds.isEmpty() || seconds.equals("null")) {
+            return "";
+        }
+        if (format == null || format.isEmpty()) {
+            format = "yyyy-MM-dd HH:mm:ss";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(Long.valueOf(seconds + "000")));
+    }
+
+    public static String date2TimeStamp(Date date, String format) {
+        if (date == null) {
+            return "";
+        }
+        if (format == null || format.isEmpty()) {
+            format = "yyyy-MM-dd HH:mm:ss";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(date);
+    }
+
+    /**
+     * 日期格式字符串转换成时间戳
+     *
+     * @param format 如：yyyy-MM-dd HH:mm:ss
+     * @return
+     */
+    public static String date2TimeStamp(String date_str, String format) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat(format);
+            return String.valueOf(sdf.parse(date_str).getTime() / 1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 取得当前时间戳（精确到秒）
+     *
+     * @return
+     */
+    public static String timeStamp() {
+        long time = System.currentTimeMillis();
+        String t = String.valueOf(time / 1000);
+        return t;
+    }
+
+    /**
+     * 取得当天0点时间戳（精确到秒）
+     *
+     * @return
+     */
+    public static long getTomorrowBegin() {
+        long now = System.currentTimeMillis() / 1000l;
+        long daySecond = 60 * 60 * 24;
+        long dayTime = now - (now + 8 * 3600) % daySecond;
+        return dayTime;
+    }
+}
---
+++ src/test/java/com/topnice/demoweb/DemowebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topnice.demoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemowebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
